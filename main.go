45d5025ec1cdb0eadd762b8109bb4781212f2da0
waire <waire.tega@gmail.com>
2022-03-15 12:56:43 +0100

    a new test channel

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 
@@ -12,6 +13,38 @@ import (
 
 type User struct {
 	UserReference string `json:"user_reference"`
+}
+type Notification struct {
+	Reference string `json:"reference" bson:"reference"`
+	Type      string `json:"type" bson:"type"`
+	Subject   string `json:"subject" bson:"subject"`
+	From      string `json:"from" bson:"from"`
+	To        string `json:"to" bson:"to"`
+	Message   string `json:"message" bson:"message"`
+	SendAt    string `json:"send_at" bson:"send_at"`
+	// SentAt string `json:"sent_at" bson:"sent_at"`
+	Status string `json:"status" bson:"status"`
+}
+type NotificationType string
+type StatusType string
+
+func (t NotificationType) CheckNotificationTypeEnum() (string, error) {
+	var response string
+	types := [...]string{"SCHEDULED", "INSTANT"}
+
+	x := string(t)
+	for _, v := range types {
+		if v == x {
+
+			response = x
+
+			return response, nil
+		}
+	}
+
+	response = ""
+
+	return response, errors.New("invalid notification type")
 }
 
 type Otp struct {
@@ -58,7 +91,7 @@ func subscribeToRedis(client *redis.Client, subChannel string) Redis {
 	return receiveMessage
 }
 
-func publishToRedis(client *redis.Client, pubChannel string, sendMessage Redis) {
+func publishToRedis(client *redis.Client, pubChannel string, sendMessage interface{}) {
 	fmt.Println("start publishing to " + pubChannel)
 	payload, err := json.Marshal(sendMessage)
 	if err != nil {
@@ -70,6 +103,7 @@ func publishToRedis(client *redis.Client, pubChannel string, sendMessage Redis) 
 	}
 	fmt.Println("end publishing to " + pubChannel)
 }
+
 func clientRedis() *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
@@ -87,13 +121,24 @@ func main() {
 		Subject:   "Publish user identity to the otp-service for identification and authorization",
 		Message:   append(identityMessage, user.UserReference),
 	}
+	sendEmail := Notification{
+		Reference: uuid.New().String(),
+		Type:      "INSTANT",
+		Subject:   "sign up email",
+		From:      "gpifedozpas@gmail.com",
+		To:        "waire.tega@gmail.com",
+		Message:   "Connect with us here",
+	}
+
 	sendOtp := Redis{
 		Reference: "868762e1-9a2b-411a-9b6c-8d518fcded91",
 		Channel:   "notification",
 		Subject:   "Send Otp to the otp-service for validation",
 		Message:   append(checkOtpMessage, "1111dshjureuj3284837hewbdsg", 620671),
 	}
-	publishToRedis(client, "identity", sendIdentity)
+	publishToRedis(client, "someService", sendIdentity)
+	time.Sleep(3 * time.Second)
+	publishToRedis(client, "identity", sendEmail)
 	subscribeToRedis(client, "otp")
 	time.Sleep(3 * time.Second)
 	publishToRedis(client, "notification", sendOtp)
